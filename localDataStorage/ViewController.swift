7cdf6d05353486bc6824b5e8ff20348836ab3f79
Patricio Vargas <patricio_vargasgonzalez@csx.com>
2018-07-02 20:16:44 -0400

    storing name in local storage

--- localDataStorage/ViewController.swift
+++ localDataStorage/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        UserDefaults.standard.set("Pato",forKey: "name")
+        
+        let name = UserDefaults.standard.object(forKey: "name")
+        
+        print(name)
     }
 
     override func didReceiveMemoryWarning() {
